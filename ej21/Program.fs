ad9cf1f45597726efdc2db46200dd50a12cf6a30
Antonio Juárez <47341777+AntonioJ156@users.noreply.github.com>
2020-05-04 21:23:12 -0500

    Edited ej21

--- ej21/Program.fs
+++ ej21/Program.fs
@@ -4,20 +4,23 @@ open System
 
 let input () =
     printf "N: "
-    let n = Convert.ToInt32(Console.ReadLine())
+    let n = Convert.ToDouble(Console.ReadLine())
     n
     
-let cmp (band, i) =
+let cmp (band, i:float) =
     if band = "T"
-        then ("F", 1/i)
-    else ("T", -1/i)
+        then ("F", 1.0/i)
+    else ("T", -1.0/i)
 
-let rec rep band serie i n =
+let rec rep band serie (i:float) n =
     match i with
     | i when i > n -> printfn "%A" serie
     | _ ->
         let (nBand, ni) = cmp (band, i)
-        rep nBand (serie+ni) (i+1) n
+        rep nBand (serie+ni) (i+1.0) n
 
 let n = input()
-rep "T" 0 1 n
+rep "T" 0.0 1.0 n
+
+
+let tmp = Console.ReadLine()
